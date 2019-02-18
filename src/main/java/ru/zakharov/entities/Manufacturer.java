4fa90de06e0e403e99dacd9ccfb13fdf3277e3ab
lexaz777 <vh1@mail.ru>
2019-02-18 21:03:44 +0300

    lesson2

---
+++ src/main/java/ru/zakharov/entities/Manufacturer.java
@@ -0,0 +1,50 @@
+package ru.zakharov.entities;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "manufacturer")
+public class Manufacturer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @OneToMany(mappedBy = "manufacturer", cascade = {CascadeType.REMOVE})
+    private List<Product> products;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Manufacturer id = " + id + ", title = " + title;
+    }
+}
+
--- src/main/java/ru/zakharov/entities/Product.java
+++ src/main/java/ru/zakharov/entities/Product.java
@@ -1,11 +1,36 @@
 package ru.zakharov.entities;
 
-public class Product {
-    private Long id;
-    private String title;
-    private Double coast;
+import javax.persistence.*;
+import java.util.List;
 
-    public Product() {
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private int price;
+
+    @ManyToOne
+    @JoinColumn(name = "manufacturer_id")
+    private Manufacturer manufacturer;
+
+    @ManyToMany
+    @JoinTable(
+            name = "clients_products",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "client_id")
+    )
+    private List<Client> clients;
+
+    public List<Client> getClients() {
+        return clients;
     }
 
     public Long getId() {
@@ -24,11 +49,27 @@ public class Product {
         this.title = title;
     }
 
-    public Double getCoast() {
-        return coast;
+    public Manufacturer getManufacturer() {
+        return manufacturer;
     }
 
-    public void setCoast(Double coast) {
-        this.coast = coast;
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public String toString() {
+        return "Product id = " + id + ", title = " + title + ", manufacturer "+ manufacturer.getTitle() + ", price = " + price;
     }
 }
