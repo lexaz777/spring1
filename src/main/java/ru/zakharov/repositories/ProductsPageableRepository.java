b92c5c59f4fecd60f2933d4f196d8b0a2b2b1e77
lexaz777 <vh1@mail.ru>
2019-02-21 23:14:39 +0300

    lesson4

---
+++ src/main/java/ru/zakharov/repositories/ProductsPageableRepository.java
@@ -0,0 +1,15 @@
+package ru.zakharov.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.stereotype.Repository;
+import ru.zakharov.entities.Product;
+
+import java.util.List;
+
+@Repository
+public interface ProductsRepository extends CrudRepository<Product,Long> {
+    public Product findProductById(Long id);
+    public List<Product> findAll();
+    public List<Product> findProductsByPriceBetween(int min,int max);
+}
